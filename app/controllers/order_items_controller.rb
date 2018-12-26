566f5872e90f61f347be65289d15919d8ffdf99c
Nelly Duclos <nellyduclos8@gmail.com>
2018-12-26 17:56:41 +0100

    créé un panier

---
+++ app/controllers/order_items_controller.rb
@@ -0,0 +1,22 @@
+class OrderItemsController < ApplicationController
+
+  def index
+    @items = current_cart.order.items
+    @total = OrderItem.all.map {|i| i.price}.sum
+  end
+
+  def create
+    current_cart.add_item(
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      user_id: params[:user_id]
+    )
+
+    redirect_to cart_path
+  end
+
+  def destroy
+    current_cart.remove_item(id: params[:id])
+    redirect_to cart_path
+  end
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,8 @@
 class Product < ApplicationRecord
+
 	belongs_to :user
-	has_many :product_categories
-	has_many :categories, through: :product_categories
+	belongs_to :category
+
 	validates :title,       presence: true
 	validates :price,       presence: true
 	validates :description, presence: true
