2fe930a6548ccc5690b55b5bba7bcc818daa9c91
Nelly Duclos <nellyduclos8@gmail.com>
2019-01-11 11:15:58 +0100

    ajout de traduction devise français et les enum de orders

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  config.i18n.default_locale = :'fr-FR'
+  config.i18n.default_locale = :fr
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
