33950070e0d49c9fdf4ccc3f27c3c33c3461eb59
Nelly Duclos <nellyduclos8@gmail.com>
2019-02-13 11:46:08 +0100

    enlève les textes de loi coté admin

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -1,39 +1,6 @@
 class Admin::PagesController < Admin::ApplicationController
-	before_action :find_customization
 	
 	def landing
-	end
-
-	def secure_payment
-	end
-
-	def delivery
-	end
-
-	def general_condition_of_sales
-
-	end
-
-	def legal_notice
-	end
-
-	def confidentiality_policy
-	end
-
-	def cookies_policy
-	end
-
-	def sizes_guide
-		#Pas encore ajouté dans la migration... ptho ? tableau texte? 
-	end
-
-	def site_map
-		#pas de custom non plus a faire à la main
-	end
-
-	private
-
-	def find_customization
 		@customization = Customization.instance
 	end
 end
