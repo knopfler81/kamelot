358cb758388aa415238c53acdd1a18fd313528bd
Nelly Duclos <nellyduclos8@gmail.com>
2019-02-16 15:58:44 +0100

    le partial journal devient une vue

--- app/assets/javascripts/dashboard.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $('.model_link').click(function() {
-    var buttonName = $(this).attr('id').split('_')[0];
-    $('#button').val(buttonName);
-    $('form').submit();
-  });
-});
