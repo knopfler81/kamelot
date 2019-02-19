4acc875a05ae97701c781ff286abfcf99ad09cdd
Nelly Duclos <nellyduclos8@gmail.com>
2019-02-19 21:34:50 +0100

    sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,17 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "super_shop_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'https://super-shop-staging.herokuapp.com/' }
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.sendgrid.net",
+    :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
+    :enable_starttls_auto => true, # detects and uses STARTTLS
+    :user_name => ENV["SENDGRID_USERNAME"],
+    :password  => ENV["SENDGRID_PASSWORD"], # SMTP password is any valid API key, when user_name is "apikey".
+    :authentication => 'login',
+    :domain => 'super-shop-staging.herokuapp.com/', # your domain to identify your server when connecting
+}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
