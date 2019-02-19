0cc36a6635f7da025072bbaf9aacc71ce4c68bcc
Nelly Duclos <nellyduclos8@gmail.com>
2019-02-19 11:40:08 +0100

    change affichage des stats

---
+++ app/assets/javascripts/highcharts_options.js
@@ -0,0 +1,13 @@
+Highcharts.setOptions({
+  lang: {
+    months: [
+        'Janvier', 'Février', 'Mars', 'Avril',
+        'Mai', 'Juin', 'Juillet', 'Août',
+        'Septembre', 'Octobre', 'Novembre', 'Décembre'
+    ],
+    weekdays: [
+        'Dimanche', 'Lundi', 'Mardi', 'Mercredi',
+        'Jeudi', 'Vendredi', 'Samedi'
+    ]
+}
+});
