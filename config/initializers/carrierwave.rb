a69f714af97da0fcd6cb69c4ed3100eeb0f5c8e8
Nelly Duclos <nellyduclos8@gmail.com>
2019-02-19 16:23:37 +0100

    vire geocoder WIP

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,27 +1,3 @@
-# if Rails.env.test?
-#   CarrierWave.configure do |config|
-#     config.storage = :file
-#     config.enable_processing = false
-#   end
-
-#   # make sure our uploader is auto-loaded
-# AttachmentUploader
-
-#   # use different dirs when testing
-#   CarrierWave::Uploader::Base.descendants.each do |klass|
-#     next if klass.anonymous?
-#     klass.class_eval do
-#       def cache_dir
-#         "spec/uploads/tmp"
-#       end
-
-#       def store_dir
-#         "public/spec/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-#       end
-#     end
-#   end
-# end
-
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
