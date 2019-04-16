2f02cf61776658941bc88ee732819ebf644616f5
Nelly <nellyduclos8@gmail.com>
2019-04-16 14:43:11 +0200

    ajoute friendly id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_15_165106) do
+ActiveRecord::Schema.define(version: 2019_04_16_120542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,17 @@ ActiveRecord::Schema.define(version: 2019_04_15_165106) do
     t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
+  end
+
   create_table "main_categories", force: :cascade do |t|
     t.string "gender"
     t.datetime "created_at", null: false
@@ -141,7 +152,9 @@ ActiveRecord::Schema.define(version: 2019_04_15_165106) do
     t.integer "supplier_id"
     t.float "weight"
     t.string "qr_code"
+    t.string "slug"
     t.index ["category_id"], name: "index_products_on_category_id"
+    t.index ["slug"], name: "index_products_on_slug", unique: true
     t.index ["supplier_id"], name: "index_products_on_supplier_id"
   end
 
---
+++ spec/factories/contacts.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :contact do
+    full_name { "MyString" }
+    email { "MyString" }
+    message_content { "MyString" }
+  end
+end
