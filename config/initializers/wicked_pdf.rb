ce78c7ac7b9e183ce85ff3ad4cda876f8f228894
nelly duclos <nellyduclos8@gmail.com>
2019-04-27 16:49:15 +0200

    conf

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -8,32 +8,32 @@
 #
 # https://github.com/mileszs/wicked_pdf/blob/master/README.md
 
+WickedPdf.config ||= {}
+# if Rails.env.production?
+#   WickedPdf.config = {
 
-if Rails.env.production?
-  WickedPdf.config = {
+#   # Path to the wkhtmltopdf executable: This usually isn't needed if using
+#   # one of the wkhtmltopdf-binary family of gems.
+#   exe_path: Gem.bin_path('wkhtmltopdf-heroku', 'wkhtmltopdf-linux-amd64'),
+#   page_size: 'Letter',
+#   orientation: 'Portrait',
+#   margin: { top:    2,
+#            bottom: 2,
+#            left:   2,
+#            right:  2 }
+#  }
 
-  # Path to the wkhtmltopdf executable: This usually isn't needed if using
-  # one of the wkhtmltopdf-binary family of gems.
-  exe_path: Gem.bin_path('wkhtmltopdf-heroku', 'wkhtmltopdf-linux-amd64'),
-  page_size: 'Letter',
-  orientation: 'Portrait',
-  margin: { top:    2,
-           bottom: 2,
-           left:   2,
-           right:  2 }
- }
-
-else
+# else
   
- WickedPdf.config = {
-    exe_path: '/usr/local/bin/wkhtmltopdf',
-    page_size: 'Letter',
-    orientation: 'Portrait',
-    margin: { top:    2,
-             bottom: 2,
-             left:   2,
-             right:  2 }
+#  WickedPdf.config = {
+#     exe_path: '/usr/local/bin/wkhtmltopdf',
+#     page_size: 'Letter',
+#     orientation: 'Landscape',
+#     margin: { top:    2,
+#              bottom: 2,
+#              left:   2,
+#              right:  2 }
 
-  }
-end
+#   }
+# end
 
