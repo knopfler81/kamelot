dd06b28130dc09accfeed2470896e1b6cec154d1
nelly duclos <nellyduclos8@gmail.com>
2019-05-07 08:50:17 +0200

    ajoute spec fav

--- spec/features/clients/favorites/favorites_spec.rb
+++ spec/features/clients/favorites/favorites_spec.rb
@@ -1,19 +1,28 @@
 require "rails_helper"
 
-RSpec.describe Favorite do 
+RSpec.describe Favorite do
+
+	before(:each) do 
+		john     = create(:user)
+		login_as john
+	end
 
 	scenario "add a product to favorites", js: true do 
 		category = create(:category, title: "Tshirt")
 		tshirt   = create(:product, brand: "Kaporal", category_id: category.id)
-		john     = create(:user)
-		login_as john
-
+		
 		visit clients_products_path
-
+		
 		first(".not-favorite").click
 
 		visit clients_favorites_path
 
 		expect(page).to have_content("KAPORAL")
 	end 
+
+	scenario "No favorite were added yet " do 
+
+		visit clients_favorites_path
+		expect(page).to have_content("Vous n'avez encore rien ajouté à votre liste de favoris")
+	end
 end
