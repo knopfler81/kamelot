9c94ceecee7747e679eff22b747935cdc8a01845
nelly duclos <nellyduclos8@gmail.com>
2019-05-07 10:08:53 +0200

    email user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,8 +3,8 @@ FactoryBot.define do
 		first_name        						{ Faker::Name.first_name + 'n'}
 		last_name         						{ Faker::Name.last_name }
 		admin             						{ [false,true ].sample }
-		sequence(:email)  						{ |n| "guest_#{n}@example.com" }
-		sequence(:email_confirmation) { |n| "guest_#{n}@example.com" }
+		sequence(:email)  						{ |n| "guest_#{n}@gmail.com" }
+		sequence(:email_confirmation) { |n| "guest_#{n}@gmail.com" }
 		birth_date        						{"20/10/1997"}
 		password          						{ 'password'}
 		list_subscriber               { true }
