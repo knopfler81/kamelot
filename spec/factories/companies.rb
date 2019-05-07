690617397386db3c5c20a6dcdfcf2b3db406a02e
nelly duclos <nellyduclos8@gmail.com>
2019-05-07 10:31:08 +0200

    test

--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -6,6 +6,6 @@ FactoryBot.define do
     city        { "MyString" }
     country     { "MyString" }
     subject_vat { false }
-    email       { 'knopfler42@gmail.com'}
+    email       { 'admin@example.com'}
   end
 end
