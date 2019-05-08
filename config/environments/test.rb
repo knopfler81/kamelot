28f8065e26d010d19b4fe6f893717adc6411372c
nelly duclos <nellyduclos8@gmail.com>
2019-05-08 14:46:00 +0200

    test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -28,15 +28,8 @@ Rails.application.configure do
 
   config.i18n.default_locale = :fr
 
-    ActionMailer::Base.smtp_settings = {
-    address:              "smtp.sendgrid.net",
-    port:                  587,
-    user_name:             ENV['SENDGRID_USERNAME'],
-    password:              ENV['SENDGRID_PASSWORD'],
-    authentication:        "plain",
-    domain:                "heroku.com",
-    enable_starttls_auto:  true
-
-  }
+  config.action_mailer.perform_deliveries = false
 
 end
+
+#testtest
