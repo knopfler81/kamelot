4f5d9450529949a61aad176e313fba3b0e5ce8a8
nelly duclos <nellyduclos8@gmail.com>
2019-05-28 13:21:56 +0200

    ajoute validation company, et fix test controller product

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Company, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	it { should validate_presence_of(:admin_name)}
+	it { should validate_presence_of(:address_1)}
+	it { should validate_presence_of(:city)}
+	it { should validate_presence_of(:zipcode)}
+	it { should validate_presence_of(:country)}
+	it { should validate_presence_of(:phone)}
+	it { should validate_presence_of(:email)}
+	it { should validate_presence_of(:site_name)}
 end
