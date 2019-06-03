b2e3d2ed84d2377e769606395cfa38516b4f19f6
nelly duclos <nellyduclos8@gmail.com>
2019-06-03 17:39:11 +0200

    fix menu et js btn top et back

--- app/assets/javascripts/scroll_btns.js
+++ app/assets/javascripts/scroll_btns.js
@@ -1,18 +1,22 @@
 $(document).on('turbolinks:load', function() {
 
-	window.onscroll = function() {scrollFunction()};
 
-	function scrollFunction() {
-	  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-	    document.getElementById("top-button").style.display = "block";
-	  } else {
-	    document.getElementById("top-button").style.display = "none";
-	  }
-	};
+ if (window.location.pathname != "/"){
 
-	$("#top-button").on('click', function(){
-	 window.scroll({top: 0, left: 0, behavior: 'smooth' });
-	 });
+		window.onscroll = function() {scrollFunction()};
 
+		function scrollFunction() {
+		  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+		    document.getElementById("top-button").style.display = "block";
+		  } else {
+		    document.getElementById("top-button").style.display = "none";
+		  }
+		};
+
+		$("#top-button").on('click', function(){
+		 window.scroll({top: 0, left: 0, behavior: 'smooth' });
+		 });
+
+	}
 });
 
