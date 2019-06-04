943e1abd2ff12c8387d557e9e4377764ad53deb7
nelly duclos <nellyduclos8@gmail.com>
2019-06-04 13:52:35 +0200

    url

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'https://lapenderiedelhomme-staging.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://lapenderie-staging.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   
