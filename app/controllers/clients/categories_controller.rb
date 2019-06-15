090c20b71ec1e92647a2cb0d257ee231cdf9c072
nelly duclos <nellyduclos8@gmail.com>
2019-06-15 18:31:44 +0200

    affiche les produits par ordre de stock

--- app/controllers/clients/categories_controller.rb
+++ app/controllers/clients/categories_controller.rb
@@ -7,7 +7,11 @@ class Clients::CategoriesController < Clients::ApplicationController
   def show
     @category = Category.friendly.find(params[:id])
     @categories = Category.all
-    @client_cat_products = @category.products.paginate(page: params[:page], per_page: 12).order('created_at DESC')
+    @client_cat_products  = @category.products.left_joins(variants: :stocks) 
+      .group(:id)
+      .select(Product.arel_table[Arel.star],
+              Stock.arel_table[:quantity].sum.as('total_quantity'))
+      .order('total_quantity ASC').paginate(page: params[:page], per_page: 12).order('created_at DESC')
   end
 
 end
