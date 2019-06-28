0d82ced916cc67a189c2eb4a820fa3ac147decbb
nelly duclos <nellyduclos8@gmail.com>
2019-06-28 10:29:50 +0200

    vire site mapé

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,13 +1,13 @@
-require 'rubygems'
-require 'sitemap_generator'
+# require 'rubygems'
+# require 'sitemap_generator'
 
-SitemapGenerator::Sitemap.default_host = "https://www.lapenderiedelhomme.fr"
+# SitemapGenerator::Sitemap.default_host = "https://www.lapenderiedelhomme.fr"
 
-# SitemapGenerator::Sitemap.create do
-#   add clients_products_path, :priority => 0.5, :changefreq => 'daily'
-#   Product.find_each do |prod|
-#     add clients_product_path(prod), :lastmod => prod.updated_at, :priority => 0.5
-#   end
-# end
+# # SitemapGenerator::Sitemap.create do
+# #   add clients_products_path, :priority => 0.5, :changefreq => 'daily'
+# #   Product.find_each do |prod|
+# #     add clients_product_path(prod), :lastmod => prod.updated_at, :priority => 0.5
+# #   end
+# # end
 
-SitemapGenerator::Sitemap.ping_search_engines
+# SitemapGenerator::Sitemap.ping_search_engines
