bb5bbe563a38167023cb53daf5352c0d91c41fcb
nelly duclos <nellyduclos8@gmail.com>
2019-06-30 17:00:51 +0200

    new sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,15 +1,11 @@
-require 'rubygems'
-require 'sitemap_generator'
-
-SitemapGenerator::Sitemap.default_host = "https://www.lapenderiedelhomme.fr"
+# Set the host name for URL creation
+SitemapGenerator::Sitemap.default_host = "http://www.lapenderiedelhomme.fr"
 
 SitemapGenerator::Sitemap.create do
-	add root_path :changefreq => 'daily'
   add clients_products_path, :priority => 0.5, :changefreq => 'daily'
+
   Product.find_each do |prod|
-    add clients_product_path(prod), :lastmod => prod.updated_at, :priority => 0.5
+    add clients_product_path(prod),:lastmod => prod.updated_at, :priority => 0.5
   end
+
 end
-
-
-SitemapGenerator::Sitemap.ping_search_engines
