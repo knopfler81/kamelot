db69737f1d98ab9ed99cf9bf44931c32e670df51
nelly duclos <nellyduclos8@gmail.com>
2019-07-03 11:27:17 +0200

    sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -5,17 +5,6 @@ require 'fog-aws'
 
 
 
-SitemapGenerator::Sitemap.default_host = "http://lapenderiedelhomme.fr"
-SitemapGenerator::Sitemap.sitemaps_host = "http://s3.eu-west-3.amazonaws.com/shopsuper/"
-SitemapGenerator::Sitemap.public_path = 'tmp/'
-SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(fog_provider: 'AWS',
-                                                                    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-                                                                    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-                                                                    fog_directory: ENV['S3_BUCKET_NAME'],
-
-                                                                    fog_region: 'eu-west-3')
-SitemapGenerator::Sitemap.sitemaps_path = 'sitemap/'
-
 SitemapGenerator::Sitemap.create do
   add clients_products_path, :priority => 0.5, :changefreq => 'daily'
 
@@ -24,4 +13,15 @@ SitemapGenerator::Sitemap.create do
   end
 
 end
+SitemapGenerator::Sitemap.default_host = "http://lapenderiedelhomme.fr"
+SitemapGenerator::Sitemap.sitemaps_host = "http://eu-west-3.amazonaws.com/shopsuper/"
+SitemapGenerator::Sitemap.public_path = 'tmp/'
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(fog_provider: 'AWS',
+                                                                    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+                                                                    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+                                                                    fog_directory: ENV['S3_BUCKET_NAME'],
+
+                                                                    fog_region: 'eu-west-3')
+SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
+
 SitemapGenerator::Sitemap.ping_search_engines('https://lapenderiedelhomme.fr/sitemap')
