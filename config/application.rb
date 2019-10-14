2b6eb881bcef0ee283e541a751fa9ea3dd306bdd
nelly duclos <nellyduclos8@gmail.com>
2019-10-14 15:37:00 +0200

    cleaning

--- config/application.rb
+++ config/application.rb
@@ -16,24 +16,14 @@ Bundler.require(*Rails.groups)
 module Kamelot
   class Application < Rails::Application
 
-
     config.x.features.send_sms         = ENV["APP_SEND_SMS"].present?
     config.x.features.stats            = ENV["APP_WITH_STATS"].present?
     config.x.features.main_categories  = ENV["APP_WITH_MAIN_CAT"].present?
-
 
     config.load_defaults 5.2
     config.active_job.queue_adapter = :sidekiq
 
     config.action_dispatch.default_headers.merge!('Cache-Control' => 'no-store, no-cache')
     config.generators.system_tests = nil
-
-    #TODO verifier mais apparement plus besoin on utilise figaro et application.yml
-    # config.before_configuration do
-    #   env_file = File.join(Rails.root, 'config', 'secrets.yml')
-    #   YAML.load(File.open(env_file)).each do |key, value|
-    #     ENV[key.to_s] = value
-    #   end if File.exists?(env_file)
-    # end
   end
 end
